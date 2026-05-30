dd83b3165c56668b40ef27d12386b7e9039d5ecc
Michelle Santoso <michellesantoso2005@gmail.com>
2026-05-30 21:58:58 +0700

    Initialize AuthModule, UsersModule, CategoriesModule, MenusModule, OrdersModule

--- quickbite-backend/src/app.module.ts
+++ quickbite-backend/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
+import { MenusModule } from './menus/menus.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, AuthModule, UsersModule, CategoriesModule, MenusModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
